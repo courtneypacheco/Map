be4235dc65358ad14a98e679b48774c34e6baf34
jtrinhx33 <jtrinh@bu.edu>
2014-11-15 02:04:35 -0500

    Add RegionRectangle.java

--- src/Rectangle/TesterClass.java
+++ src/Rectangle/TesterClass.java
@@ -1,13 +1,9 @@
 package Rectangle;
 
 public class TesterClass {
-
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Rectangle rect = new Rectangle();
 	}
 
 }
