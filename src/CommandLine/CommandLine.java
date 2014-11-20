5701ec37385ae8d6c0d1858225f5e0fec18f7d36
Minnie Kim <minniek@bu.edu>
2014-11-20 12:47:31 -0500

    Modified commandline file

--- src/CommandLine/CommandLine.java
+++ src/CommandLine/CommandLine.java
@@ -1,82 +1,43 @@
 package CommandLine;
 
+/* Command-line Interface
+ * To test on Eclipse:
+ * 		Run ---> Run Configurations ---> In the Arguments tab, enter arguments
+ * 
+ * Argument description:
+ * 		x - latitude
+ *		y - longitude
+ *		k - number of nearest counties to dislay, must be between 1-10 inclusive
+ * 
+ */
+
 public class CommandLine {
-
-	// TODO: THIS STILL NEEDS TO BE CHANGED.
+	/**
+	 * Gets called when user provides incorrect commandline args
+	 * @param errorCode: depends on the type of error from user-input
+	 */
+	static void errorCheck(int errorCode) {
+		switch (errorCode) {
+			// Incorrect number of arguments given
+			case 1: System.out.println("Error:\tincorrect number of arguments entered");
+					System.out.println("\tThis program requires three arguments in the following order:\n\t<latitude>   <longitude>   <number of results to display>\n\tExiting now.");
+					System.exit(1);
+			case 2: // out-of-bound points (ex. if user inputs 100 for the value for k)
+			case 3: // incorrect input format (ex. if user inputs "abc", "17.3zre" as latitude, longitude)
+		}
+	}
+	
 	public static void main(String[] args) {
-        
-        int argsLength = args.length;
-        String EXTENSION = ".osa";
-        int EXTENSIONLength = EXTENSION.length();
-        
-        if (argsLength > 0) {
-            
-            switch (args[0]) {
-                case "gui":
-                    // code to run gui here
-                    System.out.println("Gui runs here...");
-                    break;
-                    
-                case "encode":
-                    
-                    if (argsLength < 4 && !"--output".equals(args[argsLength - 2])){  // if "--output" flag not found, exit encoding
-                        System.out.println("Command format invalid. No '--output' flag found or not enough arguments entered.");
-                        System.out.println("Encoding failed. Exiting program...");
-                    }
-                    else {
-                        // check if output file has correct filename extension
-                        String outputFilename = args[argsLength - 1];
-                        int outputFileLength = outputFilename.length();
-                        String thisFileExtension;
-                        thisFileExtension = new String (outputFilename.substring(outputFileLength - EXTENSIONLength, outputFileLength));
-                        
-                        if (EXTENSION.equals(thisFileExtension) && outputFileLength > EXTENSIONLength){
-                            
-                            // pass all filenames to encode function
-                            String[] imageFiles;
-                            imageFiles = new String [argsLength - 3];
-
-                            for (int ii = 1; ii < argsLength - 2; ii++){
-                                imageFiles[ii - 1] = args[ii];
-                            }
-                            
-                            System.out.println("Encoding started...");
-                            //encode(imageFiles, outputFilename);
-                            System.out.println("Encoding completed...");
-                        }
-                        else {
-                            System.out.println("Output file must be specified with extension " + EXTENSION + " Please try again. Exiting program...");
-                        }
-                    }
-                    break;
-                    
-                case "view":
-                    
-                    if (argsLength != 2 || args[1].length() <= EXTENSIONLength) {
-                        System.out.println("Incorrect view command format. Exiting program...");
-                    }
-                    else {
-                        String outputFilename = args[1];
-                        int outputFileLength = outputFilename.length();
-                        
-                        String thisFileExtension;
-                        thisFileExtension = new String (outputFilename.substring(outputFileLength - EXTENSIONLength, outputFileLength));
-                        
-                        if (EXTENSION.equals(thisFileExtension)){
-                            System.out.println("Viewer launching...");
-                            //view(outputFilename);
-                        }
-                        else {
-                            System.out.println("Output file must be a " + EXTENSION + " file. Please try again. Exiting program...");
-                        }
-                    }
-                    break;
-            }
+		int argsLen = args.length;
+		
+        if (argsLen < 3) {
+        	errorCheck(1);
+        } else {
+        	String x = args[0];
+        	String y = args[1];
+    		String k = args[2];
+    		System.out.printf("Searching the %s nearest counties from point (%s, %s)...\n", k, x, y);
+    		System.exit(0);
         }
-        else {
-            System.out.println("Command not specified...");
-            System.out.println("Exiting program...");
-        }
-        
     }
 }
