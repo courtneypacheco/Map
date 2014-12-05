a77efcfb723ef0b20426261c4c3a68842f9c394b
minniek <minniek@bu.edu>
2014-12-05 18:20:54 -0500

    Update pqComparator.java

--- src/RTree/pqComparator.java
+++ src/RTree/pqComparator.java
@@ -1,21 +1,18 @@
+package RTree;
+
 import java.util.Comparator;
 
 public class pqComparator implements Comparator<Double>
 {
-
-    //d1 and d2 are distances 
-    @Override
-    public int compare(double d1, double d2)
-    {
-
-        if (d1 > d2)
-        {
+	public int compare(Double arg0, Double arg1) {
+		if (arg1 > arg0) {
             return -1;
         }
-        if (d2 < d1)
-        {
+        
+		if ( arg1  < arg0 ) {
             return 1;
         }
-        return 0;
-    }
+        
+		return 0;
+	}
 }
