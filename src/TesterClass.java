0405e193a4d9b6bf2ce87e8066500c6cedb5720f
jtrinhx33 <jtrinh@bu.edu>
2014-12-05 09:49:14 -0500

    Add mostly working RTree

--- src/TesterClass.java
+++ src/TesterClass.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 import LoadData.MapData;
 import RTree.RTreeNode;
+import RTree.RTreeNode_GlobalScale;
 import StateNeighbors.StateNeighbors;
 
 
@@ -14,53 +15,34 @@ public class TesterClass {
 		// Load map data
 		HashMap mapData_States = LoadMapData("src\\NationalFile_StateProvinceDecimalLatLong.txt");
 		
-		//TEST
-		//testcode(mapData_States);
-		
 		// Create RTree
-		RTreeNode Root = CreateRTree(mapData_States);
+		RTreeNode_GlobalScale Root = CreateRTree(mapData_States);
+		Root.printStats();	//test
 		
-		Root.printStats();
+		//-14.2975	-170.7583333
+		//ArrayList<RTreeNode_GlobalScale> nodesContainingPointTest = Root.findNodesContainingPoint(-118.809997, 46.694205); // WA
+		/*ArrayList<RTreeNode_GlobalScale> nodesContainingPointTest = Root.findNodesContainingPoint(-170.7583333, -14.2975);	// AS
+		for (int ii = 0; ii < nodesContainingPointTest.size(); ii++){
+			System.out.println(nodesContainingPointTest.get(ii).getName());
+		}
+		*/
+		
+		
+		
+		for (Object stateChild: Root.getChildren()){
+			stateChild = (RTreeNode_GlobalScale) stateChild;
+			((RTreeNode_GlobalScale) stateChild).printStats();
+			for (Object countyChild: ((RTreeNode_GlobalScale) stateChild).getChildren()){
+				countyChild = (RTreeNode_GlobalScale) countyChild;
+				((RTreeNode_GlobalScale) countyChild).printStats();
+			}
+		}
 		
 		// Load state neighbors
 		//StateNeighbors stateNeighbors = LoadStateNeighborsList();
 		
-		// Test RTreeNode methods
-		//TestRTreeNodeClass();
-	}
-	
-	public static void testcode(HashMap mapData){
-		//This prints out all the states. Just for debugging purposes.
-        for (Object current_state : mapData.keySet()) {											// Iterate through all states
-        	
-        	// IMPORTANT: Uncomment when selecting to print out a specific state (for testing purposes only)
-        	if (!current_state.toString().equals("MH")) continue;
-        	
-        	RTreeNode stateNode = new RTreeNode();												// Initialize current state's nodes
-            System.out.println(current_state);													// Prints out state
-            
-            HashMap state_value = (HashMap) mapData.get(current_state);							// Get internal state hashmap (counties and their dimensions)
-            
-            for (Object current_county : state_value.keySet()){									// Iterate through all counties in state
-            	
-                System.out.print("   - " + current_county);										// Prints out county belonging to [this] state
-                ArrayList county_dimensions = (ArrayList) state_value.get(current_county);		// Get county's list of rectangular dimensions.
-                System.out.print(" " + county_dimensions + "\n");								// Print out county's dimensions (points form)
-                
-                // Store coordinates. Note ArrayList points order: [x1, y1, x2, y2]
-                /*Double x1, x2, y1, y2;
-                x1 = (Double) county_dimensions.get(0);			// Get x1
-                y1 = (Double) county_dimensions.get(1);			// Get y1
-                x2 = (Double) county_dimensions.get(2);			// Get x2
-                y2 = (Double) county_dimensions.get(3);			// Get y2
-                
-                // Create county node: name, x1, x2, y1, y2
-                RTreeNode countyNode = new RTreeNode(current_county.toString(), x1, x2, y1, y2);
-                
-                // Add county to state
-                stateNode.addChild(countyNode);*/
-            }
-        }
+		// Test RTreeNode_GlobalScale methods
+		//TestRTreeNode_GlobalScaleClass();
 	}
 	
 	/***
@@ -94,15 +76,19 @@ public class TesterClass {
 	 * @param mapData	Type: HashMap
 	 * @return
 	 */
-	public static RTreeNode CreateRTree(HashMap<String, HashMap<String, ArrayList>> mapData){
+	public static RTreeNode_GlobalScale CreateRTree(HashMap<String, HashMap<String, ArrayList>> mapData){
 		
-		RTreeNode rootNode = new RTreeNode();			// Root of tree
+		RTreeNode_GlobalScale rootNode = new RTreeNode_GlobalScale();			// Root of tree
 		
 		//This prints out all the states. Just for debugging purposes.
         for (Object current_state : mapData.keySet()) {											// Iterate through all states
         	
-        	RTreeNode stateNode = new RTreeNode();												// Initialize current state's nodes
+        	// IMPORTANT: Uncomment when selecting to print out a specific state (for testing purposes only)
+        	//if (!current_state.toString().equals("MH")) continue;
+        	
+        	RTreeNode_GlobalScale stateNode = new RTreeNode_GlobalScale();						// Initialize current state's nodes
             System.out.println(current_state);													// Prints out state
+            stateNode.setName(current_state.toString());										// Add state's name to node
             
             HashMap state_value = (HashMap) mapData.get(current_state);							// Get internal state hashmap (counties and their dimensions)
             
@@ -115,15 +101,15 @@ public class TesterClass {
                 // Store coordinates. Note ArrayList points order: [x1, y1, x2, y2]
                 Double x1, x2, y1, y2;
                 
-                if (county_dimensions.size() < 4) continue;		// If ArrayList does not have 4 points, skip county
+                if (county_dimensions.size() != 4) continue;		// If ArrayList does not have 4 points, skip county
                 
-                x1 = (Double) county_dimensions.get(0);			// Get x1
-                y1 = (Double) county_dimensions.get(1);			// Get y1
-                x2 = (Double) county_dimensions.get(2);			// Get x2
-                y2 = (Double) county_dimensions.get(3);			// Get y2
+                y2 = (Double) county_dimensions.get(0);			// Get y2 = max latitude
+                x1 = (Double) county_dimensions.get(1);			// Get x1 = min longitude 
+                y1 = (Double) county_dimensions.get(2);			// Get y1 = min latitude
+                x2 = (Double) county_dimensions.get(3);			// Get x2 = max longitude
                 
                 // Create county node: name, x1, x2, y1, y2
-                RTreeNode countyNode = new RTreeNode(current_county.toString(), x1, x2, y1, y2);
+                RTreeNode_GlobalScale countyNode = new RTreeNode_GlobalScale(current_county.toString(), x1, x2, y1, y2);
                 
                 // Add county to state
                 stateNode.addChild(countyNode);
@@ -138,15 +124,15 @@ public class TesterClass {
 		return rootNode;
 	}
 	
-	public static void TestRTreeNodeClass(){
-		// Test RTreeNodes class
+	public static void TestRTreeNode_GlobalScaleClass(){
+		// Test RTreeNode_GlobalScales class
 		System.out.println("----------------------------------");
-		RTreeNode UnitedStates = new RTreeNode("United States", 1, 25, 1, 25);
+		RTreeNode_GlobalScale UnitedStates = new RTreeNode_GlobalScale("United States", 1, 25, 1, 25);
 		
-		RTreeNode MA = new RTreeNode("MA", 1, 10, 1, 10);
-		RTreeNode CA = new RTreeNode("CA", 1, 3, 1, 3);
-		RTreeNode TX = new RTreeNode("TX", 2, 20, 2, 20);
-		RTreeNode blah = new RTreeNode();
+		RTreeNode_GlobalScale MA = new RTreeNode_GlobalScale("MA", 1, 10, 1, 10);
+		RTreeNode_GlobalScale CA = new RTreeNode_GlobalScale("CA", 1, 3, 1, 3);
+		RTreeNode_GlobalScale TX = new RTreeNode_GlobalScale("TX", 2, 20, 2, 20);
+		RTreeNode_GlobalScale blah = new RTreeNode_GlobalScale();
 		
 		UnitedStates.addChild(MA);
 		UnitedStates.addChild(CA);
@@ -162,39 +148,41 @@ public class TesterClass {
 		// Test the expand region method
 		System.out.println("----------------------------------");
 		
-		RTreeNode Basic = new RTreeNode("Basic", 0, 10, 0, 10);
+		RTreeNode_GlobalScale Basic = new RTreeNode_GlobalScale("Basic", 0, 10, 0, 10);
 		Basic.printStats();
 		
-		RTreeNode Larger = new RTreeNode("Larger", -5, 20, -5, 20);
+		RTreeNode_GlobalScale Larger = new RTreeNode_GlobalScale("Larger", -5, 20, -5, 20);
 		Larger.printStats();
 		
-		RTreeNode Smaller = new RTreeNode("Smaller", 2, 4, 2, 4);
+		RTreeNode_GlobalScale Smaller = new RTreeNode_GlobalScale("Smaller", 2, 4, 2, 4);
 		Smaller.printStats();
 		
-		RTreeNode Both = new RTreeNode("Both", -5, 9, 1, 20);
+		RTreeNode_GlobalScale Both = new RTreeNode_GlobalScale("Both", -5, 9, 1, 20);
 		Both.printStats();
 		
 		System.out.println("----------------- Now expand Basic-----------------");
 		System.out.println("----------------- Basic + Larger -----------------");
-		Basic = new RTreeNode("Basic", 0, 10, 0, 10);	// Reset
+		Basic = new RTreeNode_GlobalScale("Basic", 0, 10, 0, 10);	// Reset
 		Basic.expandRegion(Larger);
 		Basic.printStats();
 		System.out.println("----------------- Basic + Smaller -----------------");
-		Basic = new RTreeNode("Basic", 0, 10, 0, 10);	// Reset
+		Basic = new RTreeNode_GlobalScale("Basic", 0, 10, 0, 10);	// Reset
 		Basic.expandRegion(Smaller);
 		Basic.printStats();
 		System.out.println("----------------- Basic + Both -----------------");
-		Basic = new RTreeNode("Basic", 0, 10, 0, 10);	// Reset
+		Basic = new RTreeNode_GlobalScale("Basic", 0, 10, 0, 10);	// Reset
 		Basic.expandRegion(Both);
 		Basic.printStats();
 		
 		// Test recursive find
-		Basic = new RTreeNode("Basic", 0, 10, 0, 10);	// Reset
+		Basic = new RTreeNode_GlobalScale("Basic", 0, 10, 0, 10);	// Reset
 		
-		RTreeNode hello1 = new RTreeNode("hello1", 0, 7, 0, 7);
-		RTreeNode hello2 = new RTreeNode("hello2", 0, 11, 0, 11);
-		RTreeNode hellothere1 = new RTreeNode("hellothere1", -5, 7, 0, 7);
+		RTreeNode_GlobalScale hello1 = new RTreeNode_GlobalScale("hello1", 0, 7, 0, 7);
+		RTreeNode_GlobalScale hello2 = new RTreeNode_GlobalScale("hello2", 0, 11, 0, 11);
+		RTreeNode_GlobalScale hello2baby = new RTreeNode_GlobalScale("hello2baby", 0, 11, 0, 11);
+		RTreeNode_GlobalScale hellothere1 = new RTreeNode_GlobalScale("hellothere1", -5, 7, 0, 7);
 		hello1.addChild(hellothere1);
+		hello2.addChild(hello2baby);
 		
 		Basic.addChild(hello1);
 		Basic.addChild(hello2);
@@ -207,7 +195,7 @@ public class TesterClass {
 		System.out.println("hello2 contains point (" + x + ", " + y + ")?: " + hello2.containsPoint(x, y));
 		System.out.println("hellothere1 contains point (" + x + ", " + y + ")?: " + hellothere1.containsPoint(x, y));
 		
-		ArrayList<RTreeNode> nodesContainingPointTest = Basic.findNodesContainingPoint(x, y);
+		ArrayList<RTreeNode_GlobalScale> nodesContainingPointTest = Basic.findNodesContainingPoint(x, y);
 		for (int ii = 0; ii < nodesContainingPointTest.size(); ii++){
 			System.out.println(nodesContainingPointTest.get(ii).getName());
 		}
